753c593d003a55dd800bf906a392c24503520ea9
Travis Fischer <fisch0920@gmail.com>
2020-05-31 04:58:40 -0400

    🔍

--- index.js
+++ index.js
@@ -31,64 +31,72 @@ async function main() {
     accessTokenSecret: twitterAccessTokenSecret
   })
 
-  // const { body: spotifyProfile } = await spotifyClient.getMe()
-  // console.log(spotifyProfile)
-
-  // const artist = await spotifyClient.getArtist('0yTK74zLEsMyrdVPjw3Zqi')
-  // console.log(JSON.stringify(artist, null, 2))
-
   const { id: playlistId } = spotifyClient.parse(spotifyPlaylist)
 
+  // this is just a convenience wrapper for fetching all pages of a spotify result set
   const getPlaylistTracksPaged = spotifyClient.getPagedFunc(
     spotifyClient.getPlaylistTracks.bind(spotifyClient)
   )
 
+  // get all the tracks in the target playlist
   const tracks = (await getPlaylistTracksPaged(playlistId))
     .filter((result) => !result.is_local)
     .map((result) => result.track)
-
   console.log('tracks', tracks.length)
+
+  // select a track at random from the playlist
   const track = tracks[random.int(0, tracks.length - 1)]
   console.log('track', JSON.stringify(track, null, 2))
 
+  // format and tweet a single track
   if (track) {
-    const name = sanitizeTrackName(track.name)
-    const url = track.external_urls.spotify
-    const albumName = track.album.name
-    const artists = track.artists.map((artist) => artist.name)
-
-    const numHashtags = random.int(0, 3)
-    const hasEmoji = random.float() < 0.9
-    const suffix = emoji[random.int(0, emoji.length - 1)]
-    const tags = []
-
-    const h = hashtags.slice()
-    for (let j = 0; j < numHashtags; ++j) {
-      const hashtag = h.splice(random.int(0, h.length - 1))[0]
-      if (hashtag) {
-        tags.push(hashtag)
-      }
-    }
-    const tagsString1 = tags.join(' ').trim()
-    const tagsString2 = tagsString1.length ? `${tagsString1} ` : ''
-
-    const status = `${name} by ${artists.join(' & ')}${
-      hasEmoji ? ' ' + suffix : ''
-    }\n\n${tagsString2}${url}`
-
-    console.log()
-    console.log(status)
-    console.log()
-
-    await postTweet({
-      twitterClient,
-      status
-    })
-
-    // console.log(JSON.stringify(tweet, null, 2))
+    const tweet = await formatAndTweetTrack({ twitterClient, track })
+    console.log(JSON.stringify(tweet, null, 2))
   }
 
-  // console.log(JSON.stringify(tracks, null, 2))
+  return tracks
+}
+
+function formatAndTweetTrack(opts) {
+  const { twitterClient, track } = opts
+
+  const name = sanitizeTrackName(track.name)
+  const url = track.external_urls.spotify
+  const albumName = track.album.name
+  const artists = track.artists.map((artist) => artist.name)
+
+  // TODO: in the future, it'd be great to map each artist to their respective
+  // twitter handle, so we could @mention them in the tweet
+
+  // randomize the hashtags and emoji
+  const numHashtags = random.int(0, 3)
+  const hasEmoji = random.float() < 0.9
+  const suffix = emoji[random.int(0, emoji.length - 1)]
+  const tags = []
+
+  // choose some hashtags at random
+  const h = hashtags.slice()
+  for (let j = 0; j < numHashtags; ++j) {
+    const hashtag = h.splice(random.int(0, h.length - 1))[0]
+    if (hashtag) {
+      tags.push(hashtag)
+    }
+  }
+  const tagsString1 = tags.join(' ').trim()
+  const tagsString2 = tagsString1.length ? `${tagsString1} ` : ''
+
+  // super fancy templating
+  const status = `${name} by ${artists.join(' & ')}${
+    hasEmoji ? ' ' + suffix : ''
+  }\n\n${tagsString2}${url}`
+
+  console.log()
+  console.log(status)
+  console.log()
+
+  return twitterClient.post('statuses/update', {
+    status
+  })
 }
 
 function sanitizeTrackName(name) {
@@ -96,14 +104,6 @@ function sanitizeTrackName(name) {
     .replace(/\[[^\]]*\]/g, '')
     .replace(/\([^\)]*\)/g, '')
     .trim()
-}
-
-async function postTweet(opts) {
-  const { twitterClient, status, url } = opts
-
-  return twitterClient.post('statuses/update', {
-    status
-  })
 }
 
 main().catch((err) => {
